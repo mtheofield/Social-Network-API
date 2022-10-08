d67669463cf0f2f9d4b0c47ba4d7dedff3540de7
Mary Theofield <mptheofield@gmail.com>
2022-10-08 13:51:33 -0400

    working on adding and deleting friends

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -10,6 +10,7 @@ const usersController = {
     getAllUsers(req, res) {
         Users.find({})
         .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
         .select('-__v')
         .then(dbUsersData => res.json(dbUsersData))
         .catch(err => {
@@ -20,6 +21,7 @@ const usersController = {
     getUsersById({params}, res) {
         Users.findOne({_id: params.id })
         .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
         .select('-__v')
         .then(dbUsersData => {
             if(!dbUsersData) {
@@ -57,6 +59,34 @@ const usersController = {
         })
         .catch(err => res.status(400).json(err));
     },
+
+ addFriend({params}, res) {
+    Users.findOneAndUpdate({_id: params.id}, {$push: { friends: params.friendId}}, {new: true})
+    .populate({path: 'friends', select: ('-__v')})
+    .select('-__v')
+    .then(dbUsersData => {
+        if (!dbUsersData) {
+            res.status(404).json({message: 'No User with this particular ID!'});
+            return;
+        }
+    res.json(dbUsersData);
+    })
+    .catch(err => res.json(err));
+},
+
+deleteFriend({ params }, res) {
+    Users.findOneAndUpdate({_id: params.id}, {$pull: { friends: params.friendId}}, {new: true})
+    .populate({path: 'friends', select: '-__v'})
+    .select('-__v')
+    .then(dbUsersData => {
+        if(!dbUsersData) {
+            res.status(404).json({message: 'No User with this particular ID!'});
+            return;
+        }
+        res.json(dbUsersData);
+    })
+    .catch(err => res.status(400).json(err));
+}
 
 };
 
