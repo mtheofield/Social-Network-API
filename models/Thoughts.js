bf84319837d5265f91e1dc3f51583f5764258c58
Mary Theofield <mptheofield@gmail.com>
2022-10-09 17:52:03 -0400

    updating routes to modify to work for thoughts

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,4 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
 
 const ReactionsSchema = new Schema(
     {
@@ -18,7 +19,7 @@ const ReactionsSchema = new Schema(
     createdAt: {
         type: Date,
         default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     },
 
 });
@@ -34,7 +35,7 @@ const ThoughtsSchema = new Schema(
     createdAt: {
         type: Date,
         default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
         },
     username: {
         type: String,
